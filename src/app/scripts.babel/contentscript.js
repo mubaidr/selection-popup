90b2ae5ef34376346ddc1ab2211a124b610c053a
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-12-21 15:36:16 +0500

    sahre options, prepare to add menu popup [WIP]es6 config eslint, babel

--- src/app/scripts.babel/contentscript.js
+++ src/app/scripts.babel/contentscript.js
@@ -1,3 +1,37 @@
 'use strict'
 
-console.log("'Allo 'Allo! Content script")
+// class for styling
+const class = 'selection-popup'
+
+// Get options from background script
+getOptions()
+function getOptions() {
+  chrome.runtime.sendMessage(
+    {
+      type: 'options'
+    },
+    options => {
+      prepareLayout(options)
+    }
+  )
+}
+
+// Prepare layout for popup
+function prepareLayout(options) {
+  let container = document.createElement('div')
+  container.innerHTML = `<div class='${class}-container'></div>`
+  document.appendChild(container)
+
+  prepareMenu(container)
+  prepareSearchEngines(container)
+  addEventHandlers()
+}
+
+// add menu objects to popup
+function prepareMenu() {}
+
+// add search engines objects to popup
+function prepareSearchEngines() {}
+
+// add events to document
+function addEventHandlers() {}
