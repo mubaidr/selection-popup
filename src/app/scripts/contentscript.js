9100047e23a09b86ee6728bdb1a536281ccb349c
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-12-22 18:14:33 +0500

    implement popup display [WIP] position of popup

--- src/app/scripts/contentscript.js
+++ src/app/scripts/contentscript.js
@@ -1,49 +1,68 @@
 // class for styling
-const className = 'selection-popup'
+const className = 'selection-popup-container'
+let selectionText = ''
+let options = null
 
-// add events to document
-function addEventHandlers () {
-  // TODO: add listners for text selection
+// hide popup
+function hidePopup () {
+  const popups = document.getElementsByClassName(className)
+  for (let i = 0; i < popups.length; i += 1) {
+    const popup = popups[i]
+    popup.parentNode.removeChild(popup)
+  }
 }
 
-// Prepare layout for popup
-function prepareLayout (options) {
+// show popup
+function showPopup () {
+  hidePopup()
+  if (!selectionText) return
+
   const container = document.createElement('div')
   const list = document.createElement('ul')
-  container.className = `${className}-container`
+  container.className = className
   container.appendChild(list)
   document.body.appendChild(container)
 
   Object.keys(options).forEach(key => {
     if (options[key].enabled) {
       options[key].items.forEach(item => {
-        list.innerHTML += `<li>
-          <a href=${item.url}>
+        if (item.url) {
+          list.innerHTML += `<li>
+          <a href=${item.url.replace('%s', selectionText)} target="_blank">
             ${item.name}
           </a>
         </li>`
+        } else {
+          // TODO: menu items
+        }
       })
     }
   })
-  // TODO: set position of the layout
-  /* Debug start */
-  container.className += ' active'
-  /* Debug end */
+}
 
-  addEventHandlers()
+// add mouseup event to check selection to document
+function addEventHandlers () {
+  document.onmouseup = () => {
+    const selection = window.getSelection()
+    if (selection.type === 'Range') {
+      selectionText = selection.focusNode.nodeValue
+        .substring(selection.baseOffset, selection.focusOffset)
+        .trim()
+
+      showPopup()
+    } else {
+      hidePopup()
+    }
+  }
 }
 
 // Get options from background script
 function getOptions () {
-  chrome.runtime.sendMessage(
-    {
-      type: 'options'
-    },
-    options => {
-      prepareLayout(options)
-    }
-  )
+  chrome.runtime.sendMessage({ type: 'options' }, obj => {
+    options = obj
+    addEventHandlers()
+  })
 }
 
-// TODO: Get text selection
+// Start process
 getOptions()
