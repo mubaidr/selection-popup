3ef8910c05b3c26d10f8d4edc5b430b893472bd3
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-12-26 11:49:42 +0500

    use webpack instead of gulp for vuejs (&csp)

---
+++ src/src/options/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <fieldset>
+      <legend>General</legend>
+    </fieldset>
+
+    <fieldset>
+      <legend>Menu</legend>
+    </fieldset>
+
+    <fieldset>
+      <legend>Search</legend>
+    </fieldset>
+
+    <pre>{{$data}}</pre>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        options: null
+      }
+    },
+    created () {
+      // Get options
+      chrome.runtime.sendMessage(
+        {
+          type: 'options'
+        },
+        obj => {
+          this.options = obj
+        }
+      )
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  p {
+    font-size: 20px;
+  }
+</style>
